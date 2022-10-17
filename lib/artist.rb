6ae5ffb74e942323e668d040aaf012b05854417b
Chloe Scheffler <chloescheffler@gmail.com>
2022-10-17 16:20:37 -0400

    commit tree c234d1e67d4d5d98acd779d31b3985bc88a9a33f

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,13 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
@@ -19,14 +26,6 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -34,9 +33,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
